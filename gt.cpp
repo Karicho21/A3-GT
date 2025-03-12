2e2f9e9c6a94a3c99be4bb92a4cb46ede849fe19
Karicho21 <kkato1@charlotte.edu>
2025-03-12 12:31:25 -0400

    secondpush

--- gt.cpp
+++ gt.cpp
@@ -1,90 +1,93 @@
-
 #include <curl/curl.h>
 #include <string>
 #include <iostream>
 #include <queue>
 #include <unordered_set>
 #include <chrono>
-#include <bits/stdc++.h>
+#include <vector>
+#include <rapidjson/document.h>
 
+using namespace std;
 
+// Callback function for curl to write response data into a string
 size_t my_write_data(char *ptr, size_t size, size_t nmemb, void *userdata) {
-
-    std::string* mystring = (std::string*)userdata;
-
+    string* mystring = (string*)userdata;
     for (size_t i = 0; i < nmemb; ++i) {
-        mystring -> push_back(ptr[i]);
+        mystring->push_back(ptr[i]);
     }
-    
     return nmemb;
 }
 
-vector<int> bfs(vector<vector<int>>& adj, int s)  {
-    int V = adj.size();
+// BFS function
+vector<string> bfs(string startNode, int depth) {
+    string url = "http://hollywood-graph-crawler.bridgesuncc.org/neighbors/" + startNode;
+    CURL *curl;
+    CURLcode res;
+    string readBuffer;
+    vector<string> neighbors;
 
-    vector<int> res;
-    queue<int> q;  
-    
-    // Initially mark all the vertices as not visited
-    vector<bool> visited(V, false);
-
-    // Mark source node as visited and enqueue it
-    visited[s] = true;
-    q.push(s);
-
-    // Iterate over the queue
-    while (!q.empty()) {
-      
-        // Dequeue a vertex from queue and store it
-        int curr = q.front();
-        q.pop();
-        res.push_back(curr);
-        
-        for (int x : adj[curr]) {
-            if (!visited[x]) {
-                visited[x] = true;
-                q.push(x);
-            }
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
         }
+        curl_easy_cleanup(curl);
+
+        // Parse JSON response
+        rapidjson::Document doc;
+        if (doc.Parse(readBuffer.c_str()).HasParseError()) {
+            cerr << "JSON parse error at offset " << doc.GetErrorOffset()
+                << ": " << rapidjson::GetParseErrorFunc()(doc.GetParseError()) << endl;
+            return neighbors;
+        }
+
+        if (readBuffer.empty()) {
+            cerr << "Error: Received empty response" << endl;
+            return neighbors;
+        }
+
+        if (doc.HasMember("neighbors") && doc["neighbors"].IsArray()) {
+            for (const auto& neighbor : doc["neighbors"].GetArray()) {
+                if (neighbor.IsString()) {
+                    neighbors.push_back(neighbor.GetString());
+                } else {
+                    cerr << "Error: Unexpected neighbor type" << endl;
+                }
+            }
+        } else {
+            cerr << "Error: 'neighbors' field not found or is not an array" << endl;
+        }
+        return neighbors;
     }
 
-    return res;
+    return neighbors;
 }
 
 int main(int argc, char* argv[]) {
-    // time measuring
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <startNode> <depth>\n";
+        cerr << "Usage: " << argv[0] << " <startNode> <depth>\n";
         return 1;
     }
 
-    std::string startNode = argv[1];
-    int depth = std::stoi(argv[2]);
+    string startNode = argv[1];
+    int depth = stoi(argv[2]);
 
-    auto start = std::chrono::high_resolution_clock::now();
-    bfs(startNode, depth);
-    auto end = std::chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
+    vector<string> neighbors = bfs(startNode, depth);
+    auto end = chrono::high_resolution_clock::now();
 
-    std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
+    chrono::duration<double> elapsed = end - start;
+    cout << "Time: " << elapsed.count() << " seconds\n";
 
-    //curl  part... lawd
-    CURL *curl = curl_easy_init();
-
-    curl_easy_setopt(curl, CURLOPT_URL, "http://hollywood-graph-crawler.bridgesuncc.org/neighbors/");
-    std::string myoutstring;
-
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&myoutstring);
-
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_write_data);
-
-    curl_easy_perform(curl);
-
-    std::cout<<myoutstring<<"\n";
-
-    curl_easy_cleanup(curl);
+    cout << "Neighboring node of " << startNode << ":\n";
+    for (const auto& neighbor : neighbors) {
+        cout << "  " << neighbor << endl;
+    }
 
     return 0;
-
-   
 }
+
